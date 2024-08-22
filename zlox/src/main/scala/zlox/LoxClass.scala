9fdf5fde7a00ad6602bc270671ef4d6c5ee8b40f
jon <jonathan.n@u.nus.edu>
2024-08-22 23:36:58 +0700

    methods

--- zlox/src/main/scala/zlox/LoxClass.scala
+++ zlox/src/main/scala/zlox/LoxClass.scala
@@ -1,10 +1,14 @@
 package org.zlox.zlox.Main
 
-class LoxClass(val name: String) extends LoxCallable {
+class LoxClass(val name: String, methods: Map[String, LoxFn]) extends LoxCallable {
   def arity: Int = 0
     
   def call(arguments: List[Any]): Any = {
     LoxInstance(this)
+  }
+
+  def findMethod(name: String): Option[LoxFn] = {
+    methods.get(name)
   }
 
   override def toString(): String = name
@@ -16,7 +20,9 @@ class LoxInstance(cls: LoxClass) {
   final val fields: Map[String, Any] = Map()
 
   def get(name: Token): Any = {
-    fields.getOrElse(name.lexeme, throw new RuntimeError(null, "Undefined property '" + name + "'."))
+    fields.get(name.lexeme)
+    .orElse(cls.findMethod(name.lexeme))
+    .getOrElse(throw RuntimeError(name, "Undefined property '" + name.lexeme + "'."))
   }
 
   def set(name: Token, value: Any): Unit = {
--- zlox/src/main/scala/zlox/Main.scala
+++ zlox/src/main/scala/zlox/Main.scala
@@ -677,14 +677,21 @@ object Interpreter {  // singleton
           exec(body)
         }
       }
-      case func @ Function(name, _, _) => env.define(name.lexeme, new LoxFunction(func, env))
+      case func @ Function(name, _, _) => env.define(name.lexeme, new LoxFn(func, env))
       case Return(keyword, value) => {
         val returnValue = value.map(eval).getOrElse(None)
         throw ReturnException(returnValue)
       }
       case Class(name, methods) => {
         env.define(name.lexeme, NotAssigned)
-        env.assign(name, LoxClass(name.lexeme))
+
+        val mmap: Map[String, LoxFn] = Map.from {
+          methods.map { method =>
+            (method.name.lexeme, new LoxFn(method, env))
+          }
+        }
+
+        env.assign(name, LoxClass(name.lexeme, mmap))
       }
     }
   }
@@ -840,7 +847,7 @@ abstract trait LoxCallable {
   def call(args: List[Any]): Any
 }
 
-class LoxFunction(val declaration: Function, val closure: Environment) extends LoxCallable {
+class LoxFn(val declaration: Function, val closure: Environment) extends LoxCallable {
   override def arity = declaration.params.length
 
   override def call(args: List[Any]): Any = {
@@ -857,147 +864,4 @@ class LoxFunction(val declaration: Function, val closure: Environment) extends L
   }
 
   override def toString: String = "<fn " + declaration.name.lexeme + ">"
-}
-
-object Resolver {
-  import collection.mutable.Map
-
-  var scopes = Array[Map[String, Boolean]]()
-
-  enum FnType {
-    case FUNCTION, NONE
-  }
-
-  var currentFunctionType: FnType = FnType.NONE
-
-  def resolve(thing: Stmt | Expr): Unit = {
-    thing match {
-      case Class(name: Token, methods: List[Function]) => {
-        declare(name)
-        define(name)
-      }
-      case Block(statements) => {
-        beginScope()
-        statements.foreach(resolve)
-        endScope()
-      }
-      case Var(name, initializer) => {
-        declare(name)
-        initializer.foreach(resolve)
-        define(name)
-      }
-      case expr @ Expr.Variable(name) => {
-        if (!scopes.isEmpty && scopes.last.get(name.lexeme).contains(false)) {
-          Lox.error(name.line, "Cannot read local variable in its own initializer.")
-        }
-        resolveLocal(expr, name)
-      }
-      case expr @ Expr.Assign(name, value) => {
-        resolve(value)
-        resolveLocal(expr, name)
-      }
-      case stmt @ Function(name, params, body) => {
-        declare(name)
-        define(name)
-        val enclosingFunction = currentFunctionType
-        currentFunctionType = FnType.FUNCTION
-        beginScope()
-        params.foreach { param => 
-          declare(param)
-          define(param)
-        }
-        body.foreach(resolve)
-        endScope()
-        currentFunctionType = enclosingFunction
-      }
-      case Expression(expr) => {
-        resolve(expr)
-      }
-      case If(condition, thenDo, elseDo) => {
-        resolve(condition)
-        resolve(thenDo)
-        elseDo.foreach(resolve)
-      }
-      case Print(expr) => {
-        resolve(expr)
-      }
-      case Return(keyword, value) => {
-        checkInvalidReturn(keyword)
-        value.foreach(resolve)
-      }
-      case While(condition, body) => {
-        resolve(condition)
-        resolve(body)
-      }
-      case Expr.Binary(left, operator, right) => {
-        resolve(left)
-        resolve(right)
-      }
-      case Expr.Call(callee, paren, arguments) => {
-        resolve(callee)
-        arguments.foreach(resolve)
-      }
-      case Expr.Grouping(expression) => {
-        resolve(expression)
-      }
-      case Expr.Literal(value) => ()
-      case Expr.Logical(left, operator, right) => {
-        resolve(left)
-        resolve(right)
-      }
-      case Expr.Unary(operator, right) => {
-        resolve(right)
-      }
-      case Expr.Get(expr, name) => {
-        resolve(expr)
-      }
-      case Expr.Set(obj, name, value) => {
-        resolve(obj)
-        resolve(value)
-      }
-    }
-  }
-
-  private def resolveLocal(expr: Expr, name: Token): Unit = {
-    scopes.zipWithIndex.reverse
-      .find { case (scope, _) => scope.contains(name.lexeme) }
-      .foreach { case (_, index) =>
-        Interpreter.resolve(expr, scopes.size - 1 - index)
-      }
-  }
-
-  private def checkExistingVar(name: Token) = {
-    val scope = scopes.lastOption
-    if (scope.map(_.contains(name.lexeme)).getOrElse(false)) {
-      Lox.error(name.line, "Variable with this name already declared in this scope.")
-    }
-
-    // if (scope.contains(name.lexeme)) {
-    //   Lox.error(name.line, "Variable with this name already declared in this scope.")
-    // }
-  }
-
-  private def checkInvalidReturn(name: Token) = {
-    if (scopes.isEmpty) {
-      Lox.error(name.line, "Cannot return from top-level code.")
-    }
-  }
-
-  private def declare(name: Token) = 
-    if scopes.nonEmpty then scopes.last += (name.lexeme -> false)
-  
-
-  private def define(name: Token) = 
-    if scopes.nonEmpty then scopes.last += (name.lexeme -> true)
-  
-
-  private def beginScope() = {
-    scopes = scopes.appended(Map())
-  }
-
-  private def endScope() = {
-    scopes = scopes.dropRight(1)
-  }
-
-
 }
